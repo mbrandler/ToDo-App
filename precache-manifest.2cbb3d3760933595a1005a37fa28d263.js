532e4cef25257493029a7b94e9df68a130125ccc
mbrandler <marya.brandler@yandex.ru>
2020-06-10 17:21:21 +0300

    Updates

---
+++ precache-manifest.2cbb3d3760933595a1005a37fa28d263.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8ef28cfa3f8f628a61bbdc1d10c45553",
+    "url": "/ToDo-App/index.html"
+  },
+  {
+    "revision": "639a0dfbc57a4ea25736",
+    "url": "/ToDo-App/static/css/main.fd0c39b6.chunk.css"
+  },
+  {
+    "revision": "53bf6351f5b5ee97a9bd",
+    "url": "/ToDo-App/static/js/2.8ba249f7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ToDo-App/static/js/2.8ba249f7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "639a0dfbc57a4ea25736",
+    "url": "/ToDo-App/static/js/main.a0093d81.chunk.js"
+  },
+  {
+    "revision": "2d15aa941ff16ed218ad",
+    "url": "/ToDo-App/static/js/runtime-main.1e54f1e4.js"
+  }
+]);
